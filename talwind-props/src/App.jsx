a4538db7a015730fc6ce2e5327d3bfef871face7
Neha-wadhwa <nehawadhwa7788@gmail.com>
2023-11-27 11:08:42 +0530

    tailwind complete

--- talwind-props/src/App.jsx
+++ talwind-props/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from "./Components/card"
 
 // we are using jsx neither proper html nor js for this we use jsx so
 //  some thing is different here
 //  img tag is also closing tag
 function App() {
   
-  
+  let details = {
+    name : "Neha",
+    Age : 22
+  }
+  let arr = [1,34,67]
   return (
    <>
    <h1 className = "bg-blue-300 text-black p-4 rounded-xl mb-4"> Learning Tailwind </h1>
@@ -32,25 +37,12 @@ function App() {
     </figcaption>
   </div>
 </figure>
+
+    
+ <Card username = "Neha"   myDetails= {details}  arr={arr}/>
+ <Card username = "Wadhwa" btnText = "click me "/>
 <br/>
-<div class="relative h-[400px] w-[300px] rounded-md">
-  <img
-    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-    alt="AirMax Pro"
-    class="z-0 h-full w-full rounded-md object-cover"
-  />
-  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-  <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">Delba</h1>
-    <p class="mt-2 text-sm text-gray-300">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
-    </p>
-    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
-    </button>
-  </div>
-</div>
+
 
    </>
   )
